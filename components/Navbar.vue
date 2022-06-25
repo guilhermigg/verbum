43648eacbefb21ceaf4f9871be6cc51dbe4ed706
guilhermigg <guilhermigg.e@gmail.com>
2022-06-25 16:33:55 -0300

    Feat: navbar

---
+++ components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+    <b-navbar type="is-primary">
+        <template #brand>
+            <b-navbar-item id="title" tag="router-link" :to="{ path: '/' }">
+                <h1> Verbum </h1>
+            </b-navbar-item>
+        </template>
+    </b-navbar>
+</template>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Cormorant+Garamond:wght@300&family=IBM+Plex+Sans+Arabic:wght@100;200&display=swap');
+
+#title {
+    font-family: 'Cormorant Garamond', serif;
+    font-family: 'IBM Plex Sans Arabic', sans-serif;
+    font-family: 'Playfair Display', serif;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,15 @@
 <template>
-  <div>
-  </div>
+    <div>
+        <Navbar />
+        <router-view></router-view>
+    </div>
 </template>
 
 <script>
+import Navbar from '@/components/Navbar.vue'
 export default {
   name: 'DefaultLayout',
+  components: {Navbar},
   data () {
     return {
 
